4a22bbacafe3ddaae9901434d88fcc25f396a7ed
Alexander Bogdanov <andorn@gmail.com>
2015-12-17 20:17:26 +0300

    tune sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -116,7 +116,13 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'bogus34',
+    'github_repo': 'bookshelf-schema',
+    'github_button': False,
+    'page_width': '1024px',
+    'sidebar_width': '260px'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
